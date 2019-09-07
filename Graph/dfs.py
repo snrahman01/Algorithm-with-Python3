b9be5abbc79d732652b9735b1eba9cd390ab24d1
snrahman01 <nargis.rhmn@gmail.com>
2019-09-07 19:47:10 +0600

    bfs dfs

---
+++ Graph/dfs.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def DFSUtil(self, v, visited):
+        visited[v] = True
+        print(v, end=" ")
+
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.DFSUtil(i, visited)
+
+    def DFS(self):
+        lngth = len(self.graph)
+        visited = [False] * lngth
+
+        for i in range(lngth):
+            if visited[i] == False:
+                self.DFSUtil(i, visited)
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is Depth First Traversal")
+g.DFS()
